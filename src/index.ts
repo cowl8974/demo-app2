ba2e680fc1021cc51e87ec23ab0a49b98a71f3c7
cowl8974 <robert.w.cowlishaw@gmail.com>
2022-11-25 11:18:03 -0600

    updated name

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Fuckery Sally';
+  const name = 'Fuckery Terd';
   greetUser(name);
 });
